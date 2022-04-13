9c4f272f1cdd97891eeb878acefa874d379eefef
matac42 <e185742@ie.u-ryukyu.ac.jp>
2022-04-14 00:53:56 +0900

    ...

--- atcoder/test.pl
+++ atcoder/test.pl
@@ -1,8 +1,17 @@
-use 5.10.0;
+use 5.16.0;
 use strict;
 use warnings;
 use utf8;
 
-my $answer = <>/100;
+sub matac {
+    return __SUB__;
+}
 
-say $answer;
+say __FILE__;
+say __LINE__;
+say __PACKAGE__;
+say matac;
+say <DATA>;
+
+__DATA__
+hoge
